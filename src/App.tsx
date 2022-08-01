a8c7bcd06c9faefc3f8c827011bf894b43c258e6
Circlewee <pnj1118@naver.com>
2022-08-01 16:10:14 +0900

    feat: 라우팅 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<div>App</div>} />
+        <Route path='/auth' element={<div>Login</div>} />
+        <Route path='/register' element={<div>Register</div>} />
+        <Route path='*' element={<div>404 NOT FOUND!</div>} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
