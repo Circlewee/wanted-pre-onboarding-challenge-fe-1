b28d1b80acb6fe46f57c037af76a0ceea1488725
Circlewee <pnj1118@naver.com>
2022-08-01 22:27:31 +0900

    feat: registerRequest api 추가

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,13 +1,18 @@
 import axios from 'axios';
 
-import { IUserInfo } from '@/types/types';
+import { IUserRequestSuccess, IUserInfo } from '@/types/types';
 
 const customAxios = axios.create({
-  baseURL: 'http://localhost%38080',
+  baseURL: 'http://localhost:8080',
 });
 
-export async function loginRequest(data: IUserInfo) {
-  return await customAxios.post('/users/login', data);
+export async function loginRequest(userInfo: IUserInfo) {
+  return await customAxios.post('/users/login', userInfo);
+}
+
+export async function registerRequest(userInfo: IUserInfo): Promise<IUserRequestSuccess> {
+  const { data } = await customAxios.post<IUserRequestSuccess>('/users/create', userInfo);
+  return data;
 }
 
 export default customAxios;
