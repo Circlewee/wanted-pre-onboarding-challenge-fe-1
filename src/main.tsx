358fd3256f3abffc6b9dd001ba8f121a561e068e
Circlewee <pnj1118@naver.com>
2022-08-01 16:06:56 +0900

    feat: GlobalStyle 정의

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,11 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<App />);
+import GlobalStyle from './style/GlobalStyle';
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <>
+    <GlobalStyle />
+    <App />
+  </>
+);
