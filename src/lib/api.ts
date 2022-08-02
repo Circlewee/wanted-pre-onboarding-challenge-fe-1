7209b68dc9854d03e693ac94d25550967c14d8a4
Circlewee <pnj1118@naver.com>
2022-08-02 23:58:48 +0900

    feat: todoList get 요청 header에 token 추가

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -7,7 +7,19 @@ const customAxios = axios.create({
   baseURL: 'http://localhost:8080',
 });
 
-// customAxios.interceptors.response()
+customAxios.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error?.response?.status === 400) {
+      localStorage.removeItem('token');
+      alert('접근 권한이 없습니다. 로그인 화면으로 이동합니다.');
+      window.location.href = '/auth';
+      return Promise.reject(error);
+    }
+
+    return Promise.reject(error);
+  }
+);
 
 export async function loginRequest(userInfo: IUserInfo): Promise<IUserRequestSuccess> {
   const { data } = await customAxios.post<IUserRequestSuccess>('/users/login', userInfo);
@@ -20,7 +32,10 @@ export async function registerRequest(userInfo: IUserInfo): Promise<IUserRequest
 }
 
 export async function getTodoList(): Promise<ITodoListResponse> {
-  const { data } = await customAxios.get<ITodoListResponse>('/todos');
+  const token = localStorage.getItem('token');
+  const { data } = await customAxios.get<ITodoListResponse>('/todos', {
+    headers: { Authorization: token ?? '' },
+  });
   return data;
 }
 
