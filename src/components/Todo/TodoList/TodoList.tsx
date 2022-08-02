4a34cb2ff77425d181b7d6f007161a834f2c8a89
Circlewee <pnj1118@naver.com>
2022-08-02 23:31:09 +0900

    feat: 임시 todolist get 작성

--- src/components/Todo/TodoList/TodoList.tsx
+++ src/components/Todo/TodoList/TodoList.tsx
@@ -1,7 +1,19 @@
+import { useQuery, QueryClient } from '@tanstack/react-query';
+
 import * as SC from './TodoListStyle';
+import { getTodoList } from '@/lib/api';
 
 const TodoList = () => {
-  return <SC.Wrapper>TodoList</SC.Wrapper>;
+  const queryClient = new QueryClient();
+  const { data } = useQuery(['todoList'], getTodoList);
+
+  return (
+    <SC.Wrapper>
+      {data?.data.map((todo) => {
+        return <li key={todo.id}>{todo.content}</li>;
+      })}
+    </SC.Wrapper>
+  );
 };
 
 export default TodoList;
--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,18 +1,26 @@
 import axios from 'axios';
 
 import { IUserRequestSuccess, IUserInfo } from '@/types/types';
+import { ITodoListResponse } from '@/types/todoTypes';
 
 const customAxios = axios.create({
   baseURL: 'http://localhost:8080',
 });
 
+// customAxios.interceptors.response()
+
 export async function loginRequest(userInfo: IUserInfo): Promise<IUserRequestSuccess> {
-  const { data } = await customAxios.post('/users/login', userInfo);
+  const { data } = await customAxios.post<IUserRequestSuccess>('/users/login', userInfo);
   return data;
 }
 
 export async function registerRequest(userInfo: IUserInfo): Promise<IUserRequestSuccess> {
   const { data } = await customAxios.post<IUserRequestSuccess>('/users/create', userInfo);
+  return data;
+}
+
+export async function getTodoList(): Promise<ITodoListResponse> {
+  const { data } = await customAxios.get<ITodoListResponse>('/todos');
   return data;
 }
 
