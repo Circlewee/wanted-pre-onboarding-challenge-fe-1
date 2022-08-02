f9ff8a8e89255967690c7f0b1a28502688ce2be1
Circlewee <pnj1118@naver.com>
2022-08-03 01:47:13 +0900

    feat: postTodo api 작성

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 import { IUserRequestSuccess, IUserInfo } from '@/types/types';
-import { ITodoListResponse } from '@/types/todoTypes';
+import { ITodo, ITodoListResponse, ITodoResponse } from '@/types/todoTypes';
 
 const customAxios = axios.create({
   baseURL: 'http://localhost:8080',
@@ -36,6 +36,15 @@ export async function getTodoList(): Promise<ITodoListResponse> {
   const { data } = await customAxios.get<ITodoListResponse>('/todos', {
     headers: { Authorization: token ?? '' },
   });
+  return data;
+}
+
+export async function postTodo(todo: ITodo): Promise<ITodoResponse> {
+  const token = localStorage.getItem('token');
+  const { data } = await customAxios.post<ITodoResponse>('/todos', todo, {
+    headers: { Authorization: token ?? '' },
+  });
+
   return data;
 }
 
