2120b54e9a2cc6710b772d9dcac910ed68938b21
Circlewee <pnj1118@naver.com>
2022-08-03 14:05:49 +0900

    style: api코드 들여쓰기, 띄어쓰기 수정

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,6 @@
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
 
-import { IUserRequestSuccess, IUserInfo } from '@/types/types';
+import { IUserRequestSuccess, IUserInfo, IRequestError } from '@/types/types';
 import { ITodo, ITodoListResponse, ITodoResponse } from '@/types/todoTypes';
 
 const customAxios = axios.create({
@@ -9,8 +9,8 @@ const customAxios = axios.create({
 
 customAxios.interceptors.response.use(
   (response) => response,
-  (error) => {
-    if (error?.response?.status === 400) {
+  (error: AxiosError<IRequestError>) => {
+    if (error.response?.status === 400) {
       localStorage.removeItem('token');
       alert('접근 권한이 없습니다. 로그인 화면으로 이동합니다.');
       window.location.href = '/auth';
@@ -23,26 +23,33 @@ customAxios.interceptors.response.use(
 
 export async function loginRequest(userInfo: IUserInfo): Promise<IUserRequestSuccess> {
   const { data } = await customAxios.post<IUserRequestSuccess>('/users/login', userInfo);
+
   return data;
 }
 
 export async function registerRequest(userInfo: IUserInfo): Promise<IUserRequestSuccess> {
   const { data } = await customAxios.post<IUserRequestSuccess>('/users/create', userInfo);
+
   return data;
 }
 
 export async function getTodoList(): Promise<ITodoListResponse> {
   const token = localStorage.getItem('token');
   const { data } = await customAxios.get<ITodoListResponse>('/todos', {
-    headers: { Authorization: token ?? '' },
+    headers: {
+      Authorization: token ?? '',
+    },
   });
+
   return data;
 }
 
 export async function postTodo(todo: ITodo): Promise<ITodoResponse> {
   const token = localStorage.getItem('token');
   const { data } = await customAxios.post<ITodoResponse>('/todos', todo, {
-    headers: { Authorization: token ?? '' },
+    headers: {
+      Authorization: token ?? '',
+    },
   });
 
   return data;
