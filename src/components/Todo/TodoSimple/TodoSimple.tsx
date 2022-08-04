a61fb40f704591bc44ec37e82f956770bb6aeb78
Circlewee <pnj1118@naver.com>
2022-08-04 21:39:17 +0900

    feat: 완료 표시기능 추가(checkbox)

--- src/components/Todo/TodoSimple/TodoSimple.tsx
+++ src/components/Todo/TodoSimple/TodoSimple.tsx
@@ -1,4 +1,5 @@
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 import * as SC from './TodoSimpleStyle';
 import { ITodo } from '@/types/todoTypes';
@@ -11,6 +12,7 @@ interface ITodoSimpleProps {
 
 const TodoSimple = ({ todo, active, deleteRequest }: ITodoSimpleProps) => {
   const navigate = useNavigate();
+  const [isDone, setIsDone] = useState(false);
 
   function handleDelete() {
     deleteRequest(todo.id);
@@ -22,9 +24,14 @@ const TodoSimple = ({ todo, active, deleteRequest }: ITodoSimpleProps) => {
     }
   }
 
+  function handleChange() {
+    setIsDone((prev) => !prev);
+  }
+
   return (
     <SC.Wrapper>
-      <SC.Todo onClick={goDetail} active={active}>
+      <input type='checkbox' onChange={handleChange} />
+      <SC.Todo onClick={goDetail} active={active} isDone={isDone}>
         {todo.title}: {todo.content}
       </SC.Todo>
       <button onClick={handleDelete}>❌</button>
--- src/components/Todo/TodoSimple/TodoSimpleStyle.ts
+++ src/components/Todo/TodoSimple/TodoSimpleStyle.ts
@@ -1,13 +1,25 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 export const Wrapper = styled.li`
+  font-size: 1.2rem;
+
   & + & {
     margin-top: 0.5rem;
   }
+
+  > button {
+    font-size: 1.2rem;
+  }
 `;
 
-export const Todo = styled.span<{ active: boolean }>`
+export const Todo = styled.span<{ active: boolean; isDone: boolean }>`
   padding: 3px;
   border-bottom: 1px solid ${(props) => (props.active ? '#000' : '#fff')};
   cursor: ${(props) => (props.active ? 'default' : 'pointer')};
+
+  ${(props) =>
+    props.isDone &&
+    css`
+      text-decoration: line-through;
+    `}
 `;
