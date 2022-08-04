212e953485813f19cf0e4201343499e4e6cebb73
Circlewee <pnj1118@naver.com>
2022-08-04 19:42:51 +0900

    feat: TodoForm에 default, cancelUpdate props 추가

--- src/components/Todo/TodoForm/TodoForm.tsx
+++ src/components/Todo/TodoForm/TodoForm.tsx
@@ -1,37 +1,45 @@
 import { useForm } from 'react-hook-form';
 
-import { ITodo } from '@/types/todoTypes';
+import { ITodo, IFormType } from '@/types/todoTypes';
 import * as SC from './TodoFormStyle';
 
 interface IFormProps {
-  request: (data: ITodo) => void;
+  request: (data: IFormType, id?: string) => void;
   title: string;
+  default?: ITodo;
+  cancelUpdate?: () => void;
 }
 
-const TodoForm = ({ request, title }: IFormProps) => {
+const TodoForm = (props: IFormProps) => {
   const { register, handleSubmit, reset } = useForm<ITodo>({ mode: 'onChange' });
 
-  function onSubmit(data: ITodo) {
-    request(data);
+  function onSubmit(data: IFormType) {
+    if (props.default) {
+      props.request(data, props.default.id);
+    }
+    props.request(data);
     reset({ title: '', content: '' });
   }
 
   return (
     <SC.Form onSubmit={handleSubmit(onSubmit)}>
-      <h2>{title}</h2>
+      <h2>{props.title}</h2>
       <SC.TodoInput
         id='todoTitle'
         type='text'
         placeholder='Title'
+        defaultValue={props.default?.title}
         {...register('title', { required: true })}
       />
       <SC.TodoInput
         id='todoContent'
         type='text'
         placeholder='Content'
+        defaultValue={props.default?.content}
         {...register('content', { required: true })}
       />
       <SC.PostButton type='submit'>추가</SC.PostButton>
+      {props.cancelUpdate && <SC.PostButton onClick={props.cancelUpdate}>취소</SC.PostButton>}
     </SC.Form>
   );
 };
