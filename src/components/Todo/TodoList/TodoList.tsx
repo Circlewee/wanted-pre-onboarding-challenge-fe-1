bef926f58aa0183adc44ae2b7110fe038c4de9a6
Circlewee <pnj1118@naver.com>
2022-08-04 19:42:13 +0900

    feat: QueryClient를 사용하는 것을 refetch()로 변경

--- src/components/Todo/TodoList/TodoList.tsx
+++ src/components/Todo/TodoList/TodoList.tsx
@@ -5,7 +5,7 @@ import { useNavigate, useParams } from 'react-router-dom';
 import * as SC from './TodoListStyle';
 import { getTodoList, postTodo, deleteTodo } from '@/lib/api';
 import { IRequestError } from '@/types/types';
-import { ITodo, ITodoListResponse, ITodoResponse } from '@/types/todoTypes';
+import { IFormType, ITodoResponse } from '@/types/todoTypes';
 import TodoSimple from '../TodoSimple/TodoSimple';
 import TodoForm from '../TodoForm/TodoForm';
 
@@ -13,40 +13,44 @@ const TodoList = () => {
   const navigate = useNavigate();
   const params = useParams();
 
-  const queryClient = useQueryClient();
-  const { data } = useQuery(['todoList'], getTodoList, {
+  // const queryClient = useQueryClient();
+  const { data, refetch } = useQuery(['todoList'], getTodoList, {
     staleTime: 5000,
+    suspense: true,
   });
 
-  const postMutation = useMutation<ITodoResponse, AxiosError<IRequestError>, ITodo>(postTodo, {
+  const postMutation = useMutation<ITodoResponse, AxiosError<IRequestError>, IFormType>(postTodo, {
     onError(error, variables, context) {
-      console.log(error);
+      alert(error);
     },
     onSuccess(response) {
       /* setQueryData와 invalidateQueries는 같은 기능을 한다.
        * 다만 invalidateQueries는 적은 코드, 요청 1회 추가
-       * setQueryData는 좀 더 많은 코드, 요청 없음, 응답으로 필드를 컨트롤 할 수 있음 이라는 차이점이 있다 */
-      queryClient.setQueryData(
-        ['todoList'],
-        (old: ITodoListResponse | undefined): ITodoListResponse => {
-          if (old) {
-            return { data: [...old.data, response.data] };
-          }
-          return { data: [response.data] };
-        }
-      );
+       * setQueryData는 좀 더 많은 코드, 요청 없음, 응답으로 필드를 컨트롤 할 수 있음 이라는 차이점이 있다.
+       * 혹은 refetch()도 존재한다. */
+      // queryClient.invalidateQueries(['todoList']);
+      // queryClient.setQueryData(
+      //   ['todoList'],
+      //   (old: ITodoListResponse | undefined): ITodoListResponse => {
+      //     if (old) {
+      //       return { data: [...old.data, response.data] };
+      //     }
+      //     return { data: [response.data] };
+      //   }
+      // );
+      refetch();
       navigate(`/${response.data.id}`);
     },
   });
 
   const deleteMutation = useMutation(deleteTodo, {
     onSuccess() {
-      queryClient.invalidateQueries(['todoList']);
-      navigate(-1);
+      refetch();
+      navigate('/');
     },
   });
 
-  function postRequest(data: ITodo) {
+  function postRequest(data: IFormType) {
     postMutation.mutate(data);
   }
 
