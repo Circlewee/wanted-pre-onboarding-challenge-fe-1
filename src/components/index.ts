acb39d154009ac5f72b9e9a63631d7bd1a01bb3e
Circlewee <pnj1118@naver.com>
2022-08-05 18:37:48 +0900

    chore: 사용하지 않는 모달 코드 삭제

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,5 @@
 import UserForm from './UserForm/UserForm';
-import AlertModal from './AlertModal/AlertModal';
 import * as Todo from './Todo';
 import Loading from './Loading/Loading';
 
-export { UserForm, AlertModal, Todo, Loading };
+export { UserForm, Todo, Loading };
