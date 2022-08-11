999a4f16708311dcf0bcb9d7ed50cbe6e3785072
Circlewee <pnj1118@naver.com>
2022-08-11 15:40:34 +0900

    refactor: GlobalStyle.ts 들여쓰기 수정

--- src/style/GlobalStyle.ts
+++ src/style/GlobalStyle.ts
@@ -1,12 +1,12 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-@font-face {
+  @font-face {
     font-family: 'MICEGothic';
     src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2206-01@1.0/MICEGothic.woff2') format('woff2');
     font-weight: 400;
     font-style: normal;
-}
+  }
 
   *, *::before, *::after {
     box-sizing: border-box;
